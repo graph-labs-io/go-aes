ba6f8eb8c8734230be08fc48a846ca1960596945
FlowGoCrazy <thatflowgocrazy@gmail.com>
2021-08-08 19:32:40 -0400

    init

---
+++ aes.go
@@ -0,0 +1,103 @@
+package aes
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/mergermarket/go-pkcs7"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func AesEnryptionHelper(plaintext string) (ciphertext string, err error) {
+	byteKey, decodeErr := hex.DecodeString(os.Getenv("AES_KEY"))
+	if decodeErr != nil {
+		return "", decodeErr
+	}
+
+	aesEncryptedString, encryptionErr := Encrypt(plaintext, byteKey)
+	if encryptionErr != nil {
+		return "", encryptionErr
+	}
+
+	return aesEncryptedString, nil
+}
+
+func AesDecryptionHelper(ciphertext string) (plaintext string, err error) {
+	byteKey, decodeErr := hex.DecodeString(os.Getenv("AES_KEY"))
+	if decodeErr != nil {
+		return "", decodeErr
+	}
+
+	aesDecryptedString, decryptionErr := Decrypt(ciphertext, byteKey)
+	if decryptionErr != nil {
+		return "", decryptionErr
+	}
+
+	return aesDecryptedString, nil
+}
+
+func Encrypt(unencrypted string, key []byte) (string, error) {
+	plainText := []byte(unencrypted)
+	plainText, err := pkcs7.Pad(plainText, aes.BlockSize)
+	if err != nil {
+		return "", fmt.Errorf(`plainText: "%s" has error`, plainText)
+	}
+	if len(plainText)%aes.BlockSize != 0 {
+		return "", fmt.Errorf(`plainText: "%s" has the wrong block size`, plainText)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	cipherText := make([]byte, aes.BlockSize+len(plainText))
+	iv := cipherText[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(cipherText[aes.BlockSize:], plainText)
+
+	return fmt.Sprintf("%x", cipherText), nil
+}
+
+func Decrypt(encrypted string, key []byte) (string, error) {
+	cipherText, decodeStringErr := hex.DecodeString(encrypted)
+	if decodeStringErr != nil {
+		return "", decodeStringErr
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	if len(cipherText) < aes.BlockSize {
+		return "", fmt.Errorf("cipherText too short")
+	}
+	iv := cipherText[:aes.BlockSize]
+	cipherText = cipherText[aes.BlockSize:]
+	if len(cipherText)%aes.BlockSize != 0 {
+		return "", fmt.Errorf("cipherText is not a multiple of the block size")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(cipherText, cipherText)
+
+	cipherText, unpadErr := pkcs7.Unpad(cipherText, aes.BlockSize)
+	if unpadErr != nil {
+		return "", unpadErr
+	}
+
+	cipherText = bytes.Trim(cipherText, "\x00")
+	return fmt.Sprintf("%s", cipherText), nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/graph-labs-io/go-aes
+
+go 1.16
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/mergermarket/go-pkcs7 v0.0.0-20170926155232-153b18ea13c9
+)
